dcc374e88b838915c7b7abe30caa514099c683e5
Muhammad Ayaaz <146196601+Muhammad-ayaaz@users.noreply.github.com>
2024-05-23 13:13:27 +0300

    FYP
    Weapon Detection Model all files included

---
+++ MainPortalScript.js
@@ -0,0 +1,20 @@
+const OnClickPersonalLogin = () => {
+    window.location.href = "PersonalLogin.html";
+}
+
+const OnClickInstitutionalLogin = () => {
+    window.location.href = "/InstitutionalLogin.html";
+}
+
+const OnClickInstitutionalLogin_2 = () => {
+    window.location.href = "/OtherProject/SecurityPanel.html";
+}
+
+const OnClickAdminLogin = () => {
+    window.location.href = "/OtherProject/index.html";
+    return false
+}
+
+const OnClickInstitutionalLoginButton = () => {
+    window.location.href = "/OtherProject/SecurityPanel.html";
+}
---
+++ Readme_2.txt
@@ -0,0 +1,8 @@
+alert sy live alerts ayen gay
+
+camera feed main monitoring ho gee
+
+noitifications pe alerts k notification ayen gay
+
+records main alerts ki database ho gee
+
